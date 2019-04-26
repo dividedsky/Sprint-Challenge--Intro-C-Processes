b72ad6c40299b70afb64cae91331f568dac32088
Justin <jdlow2@gmail.com>
2019-04-26 11:57:52 -0400

    working on printing file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,18 +2,19 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 
-void get_dir(char *dir_name);
+/* void get_dir(char *dir_name); */
 
 int main(int argc, char **argv)
 {
   // Parse command line
   char *dir_name = malloc(100);
-  printf("%d args supplied\n", argc);
+  /* printf("%d args supplied\n", argc); */
   if (argc == 1) {
     dir_name = ".";
   } else if (argc == 2) {
@@ -22,8 +23,9 @@ int main(int argc, char **argv)
     printf("Too many arguments specified: Usage: ./lsls [directory]\n");
     exit(1);
   }
+
   /*
-   // input function not needed as we're parsing the command line
+   // whoops: this input function not needed as we're parsing the command line
   char *dir_name = malloc(100);
   get_dir(dir_name);
   printf("dir_name in main set as %s\n", dir_name);
@@ -40,19 +42,31 @@ int main(int argc, char **argv)
 
   // Repeatly read and print entries
   while ((file = readdir(dir)) != NULL) {
-    printf("%s\n", file->d_name);
+    char *full_path = malloc(100);
+    full_path = strcat(full_path, file->d_name);
+    struct stat *statbuf = malloc(sizeof(struct stat));
+    /* printf("%d\n", stat(full_path, statbuf)); */
+    stat(full_path, statbuf);
+    printf("file size is %10ld\n", statbuf->st_size);
+
+    printf("full path is %s\n", full_path);
+    printf("file name is %s\n", file->d_name);
+    free(full_path);
+    free(statbuf);
+    /* printf("%s\n", file->d_name); */
   }
 
   // Close directory
-  closedir(dir);
+  /* closedir(dir); */
 
   return 0;
 }
 
+/*
 void get_dir(char *dir_name)
 {
-  /* accepts a pointer to a character string, reads input until 
-     \n or EOF is encountered and updates dir_name */
+  // accepts a pointer to a character string, reads input until 
+   //  \n or EOF is encountered and updates dir_name 
   int i = 0;
   char c;
   while((c = getc(stdin)) != EOF && c != '\n') {
@@ -61,3 +75,4 @@ void get_dir(char *dir_name)
   }
   printf("dir name set to %s\n", dir_name);
 }
+*/
