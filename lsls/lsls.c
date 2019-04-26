6b322f24eb1a2b4bbd5c79300c1f55b3346b294f
Justin <jdlow2@gmail.com>
2019-04-26 13:38:11 -0400

    code cleanup and comments for clarity

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,34 +9,28 @@
  * Main
  */
 
-/* void get_dir(char *dir_name); */
 
 int main(int argc, char **argv)
 {
   // Parse command line
   char *dir_name = malloc(100);
-  /* printf("%d args supplied\n", argc); */
   if (argc == 1) {
-    /* dir_name = "."; */
+    // if there are no arguments, set dir_name to current path
     getcwd(dir_name, 100);
-    printf("dir name is %s\n", dir_name);
   } else if (argc == 2) {
+    // otherwise, use the provided path
     dir_name = argv[1];
   } else {
+    // improper usage
     printf("Too many arguments specified: Usage: ./lsls [directory]\n");
     exit(1);
   }
 
-  /*
-   // whoops: this input function not needed as we're parsing the command line
-  char *dir_name = malloc(100);
-  get_dir(dir_name);
-  printf("dir_name in main set as %s\n", dir_name);
-  */
 
   // Open directory
   DIR *dir = opendir(dir_name);
   if (dir == NULL) {
+    // directory not found, print error and exit
     printf("that directory does not exist\n");
     exit(1);
   }
@@ -48,56 +42,32 @@ int main(int argc, char **argv)
   while (file) {
     struct stat statbuf;
     char full_path[512];
-    int is_dir;
+    int is_dir; // to keep track of whether this is a directory
+
+    // construct full path
     if (argc == 1) {
-      /* full_path = strcat(dir_name, file->d_name); */
       sprintf(full_path, "%s/%s", dir_name, file->d_name);
     }
     else {
-      /* full_path = strcat(argv[1], "/"); */
-      /* full_path = strcat(full_path, file->d_name); */
       sprintf(full_path, "%s/%s", argv[1], file->d_name);
     }
-    /* printf("%d\n", stat(full_path, statbuf)); */
-    /* printf("full path is %s\n", full_path); */
-    stat(full_path, &statbuf);
-    /* if ((statbuf.st_mode&S_IFDIR) != 0) { */
-    /*   is_dir = 1; */
-    /* } */
-    is_dir = statbuf.st_mode&S_IFDIR;
-    /* printf("file size is %10ld\n", statbuf.st_size); */
 
-    /* printf("full path is %s\n", full_path); */
+    stat(full_path, &statbuf);
+
+    // use the bitwise AND op to check if this is a directory
+    is_dir = statbuf.st_mode&S_IFDIR;
     if (is_dir) {
       printf("%-10s%s\n", "<DIR>", file->d_name);
     } else {
       printf("%-10ld%s\n",  statbuf.st_size, file->d_name);
     }
-    /* printf("mode is %d\n", statbuf.st_mode&S_IFDIR); */
-    /* printf("%s\n", file->d_name); */
-    memset(full_path, 0, strlen(full_path));
+
+    // update file for next loop
     file = readdir(dir);
   }
-    /* free(full_path); */
 
   // Close directory
   closedir(dir);
-  /* free(full_path); */
 
   return 0;
 }
-
-/*
-void get_dir(char *dir_name)
-{
-  // accepts a pointer to a character string, reads input until 
-   //  \n or EOF is encountered and updates dir_name 
-  int i = 0;
-  char c;
-  while((c = getc(stdin)) != EOF && c != '\n') {
-    *(dir_name + i) = c;
-    i++;
-  }
-  printf("dir name set to %s\n", dir_name);
-}
-*/
