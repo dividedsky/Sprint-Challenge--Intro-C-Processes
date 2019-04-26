a2eae06cfaeab89e463853d17cd1696cb74155bb
Justin <jdlow2@gmail.com>
2019-04-26 10:58:33 -0400

    read directory from command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
+
+void get_dir(char *dir_name);
+
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir_name = malloc(100);
+  get_dir(dir_name);
+  printf("dir_name in main set as %s\n", dir_name);
 
   // Open directory
 
@@ -15,4 +22,17 @@ int main(int argc, char **argv)
   // Close directory
 
   return 0;
+}
+
+void get_dir(char *dir_name)
+{
+  /* accepts a pointer to a character string, reads input until 
+     \n or EOF is encountered and updates dir_name */
+  int i = 0;
+  char c;
+  while((c = getc(stdin)) != EOF && c != '\n') {
+    *(dir_name + i) = c;
+    i++;
+  }
+  printf("dir name set to %s\n", dir_name);
 }
